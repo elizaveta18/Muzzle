9c121c32bf85b3c586b858dc58b96dbfef0e9c12
lizka.flex@outlook.com <lizka.flex@outlook.com>
2022-03-10 10:36:31 +0300

    Добавьте файлы проекта.

---
+++ Muzzle/Class/BaseConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Muzzle.Class
+{
+    class BaseConnect
+    {
+        public static Entities2 baseConnect;
+    }
+}
